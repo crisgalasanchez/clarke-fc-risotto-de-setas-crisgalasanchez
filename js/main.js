852f14d81accdd5be62618639b8a7af14a6d9023
cristina <crisgalasanchez@gmail.com>
2018-04-06 23:04:33 +0200

    some styles

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 'use strict';
 var productsList = document.querySelector('.products__list');
+var titleRecipe = document.querySelector('.header__title');
+var counterProducts = 0;
 
 function requestIngredients(){
   fetch('https://raw.githubusercontent.com/Adalab/recipes-data/master/rissoto-setas.json')
@@ -11,19 +13,20 @@ function requestIngredients(){
   });
   return responseJSON;
 }
-function ListIngredient(){
-  productsList.innerHTML ="";
+
+function ListIngredient(responseJSON){
+  // productsList.innerHTML ="";
 	var list = '';
   var recipe = responseJSON.recipe.ingredients;
   for(var i=0; i < recipe.length; i++){
-		list+= '<li class="">';
+		list+= '<li class="products__list">';
     list+= '<input class="checkbox__list" type="checkbox" value="'+recipe[i].product+'" name="ingredients"/>';
-    list+= '<div class="">';
     list+= '<p class="counter__list"></p>';
-    list+= '<div class="">';
+    list+= '<div class="list__details">';
     list+= '<h3 class="article__list">'+recipe[i].product+'</h3>';
     list+= '<h4 class="brand__list">'+recipe[i].brand+'</h4>';
     list+= '<p class="weight__list">'+recipe[i].quantity+'</p>';
+    list+= '</div>';
     list+= '<div class="">';
     list+= '<p class="price__list">'+recipe[i].price+''+recipe.currency+'</p>';
     list+= '</div>';
@@ -31,4 +34,5 @@ function ListIngredient(){
 	}
     productsList.innerHTML +=list;
     console.log('entro en el ListIngredient')
+    titleRecipe.innerHTML = recipe.name
 }
